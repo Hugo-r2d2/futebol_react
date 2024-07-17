937894e7560edcc2b7a2d96169ca1e1fdf62fd18
Hugo Camelo dos Santos <ihugocs@gmail.com>
2024-07-17 11:34:08 -0400

    Update Page

--- src/components/TimesList.js
+++ src/components/TimesList.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import AddTime from "./AddTime";
+import EditTime from "./EditTime";
 
 const headers = {
   "ngrok-skip-browser-warning": "123",
@@ -8,6 +9,7 @@ const headers = {
 
 function TimesList() {
   const [times, setTimes] = useState([]);
+  const [editTime, setEditTime] = useState(null);
 
   const fetchTimes = async () => {
     try {
@@ -28,6 +30,24 @@ function TimesList() {
     setTimes([...times, novoTime]);
   };
 
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`https://6349-45-236-9-52.ngrok-free.app/times/${id}`);
+      setTimes(times.filter((time) => time.id !== id));
+    } catch (error) {
+      console.error("Erro ao remover time:" + error);
+    }
+  };
+
+  const handleEdit = (time) => {
+    setEditTime(time);
+  };
+
+  const handleTimeUpdate = (edTime) => {
+    //console.log('Time atualizado'+edTime.nome);
+    setTimes(times.map((t) => (t.id === edTime.id ? edTime : t)));
+  };
+
   useEffect(() => {
     fetchTimes();
   }, []);
@@ -35,12 +55,14 @@ function TimesList() {
     <>
       <h2>Tabela de times</h2>
       <AddTime onTimeAdded={handleTimeAdded} />
+      <EditTime time={editTime} onTimeUpdated={handleTimeUpdate} />
       <table>
         <thead>
           <tr>
             <th>ID</th>
             <th>Nome do time</th>
             <th>Títulos</th>
+            <th>Ações</th>
           </tr>
         </thead>
         <tbody>
@@ -49,6 +71,12 @@ function TimesList() {
               <td>{time.id}</td>
               <td>{time.nome}</td>
               <td>{time.titulos}</td>
+              <td>
+                <button onClick={() => handleDelete(time.id)}>Remover </button>{" "}
+              </td>
+              <td>
+                <button onClick={() => handleEdit(time)}>Editar</button>{" "}
+              </td>
             </tr>
           ))}
         </tbody>
