3cfa221d752298ac4254bf3b4a14932d01d9c462
Hugo Camelo dos Santos <ihugocs@gmail.com>
2024-07-17 09:41:14 -0400

    criando projeto

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import TimesList from "./components/TimesList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lista de Times de Futebol</h1>
+      <p>Aqui fica a lista de times</p>
+      <TimesList />
+    </div>
+  );
+}
+
+export default App;
